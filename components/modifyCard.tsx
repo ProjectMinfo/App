a3d2986c8fc1c385e0fc3e0aaae5a176a3e7b699
MiloMontuori <milo.montuori@student.junia.com>
2024-05-27 16:23:18 +0200

    add types + d'autres trucs

--- components/modifyCard.tsx
+++ components/modifyCard.tsx
@@ -1,28 +1,47 @@
 'use client';
 import { Card, Button } from '@nextui-org/react';
 
-interface ModifyCardProps {
-  title: string;
-  description: string;
+interface Menu {
+    commentaire: string;
+    dispo: boolean;
+    id: number;
+    nom: string;
+    prix: number;
+    prixServeur: number;
+    quantiteBoisson: number;
+    quantitePlat: number;
+    quantiteSnack: number;
 }
 
-export default function ModifyCard({ title, description }: ModifyCardProps) {
-  return (
-    <Card className="p-4 relative my-3">
-      <div className="flex">
-        <div className="flex-1">
-          <h4>{title}</h4>
-          <p className="my-2">{description}</p>
-        </div>
-        <div className="flex flex-col">
-          <Button>
-            Modifier
-          </Button>
-          <Button color="danger" className="">
-            Supprimer
-          </Button>
-        </div>
-      </div>
-    </Card>
-  );
+interface ModifyCardProps {
+    menu: Menu;
+}
+
+export default function ModifyCard({ menu }: ModifyCardProps) {
+    return (
+        <Card className="p-4 relative">
+            <div className="flex flex-col gap-4">
+                <div className="flex justify-between items-center">
+                    <p className="text-xl font-semibold">{menu.nom}</p>
+                    <div className="flex gap-2">
+                        <Button>
+                            Modifier
+                        </Button>
+                        <Button color="danger">
+                            Supprimer
+                        </Button>
+                    </div>
+                </div>
+                <div className=" p-4 rounded-md">
+                    <p className="text-gray-700"><strong>Disponibilité:</strong> {menu.dispo ? 'Oui' : 'Non'}</p>
+                    <p className="text-gray-700"><strong>Prix:</strong> {menu.prix} €</p>
+                    <p className="text-gray-700"><strong>Prix Serveur:</strong> {menu.prixServeur} €</p>
+                    <p className="text-gray-700"><strong>Quantité Boisson:</strong> {menu.quantiteBoisson}</p>
+                    <p className="text-gray-700"><strong>Quantité Plat:</strong> {menu.quantitePlat}</p>
+                    <p className="text-gray-700"><strong>Quantité Snack:</strong> {menu.quantiteSnack}</p>
+                    <p className="text-gray-700"><strong>Commentaire:</strong> {menu.commentaire}</p>
+                </div>
+            </div>
+        </Card>
+    );
 };
--- config/api.ts
+++ config/api.ts
@@ -45,6 +45,17 @@ export const getIngredients = async (id?: number) => {
 }
 
 
+export const getMenus = async () => {
+    try {
+        const response = await api.get('/menus');
+        return (response.data);
+    } catch (error) {
+        console.error('Error fetching menus:', error);
+        throw error;
+    }
+}
+
+
 export const postViandes = async (data: any) => {
     const token = 'DEV_TOKEN'; // Add your token here
 
