0d57c4b36a1a04557b9f11cf4d048f52f0f3bae9
HosteMatthieu <90179670+HosteMatthieu@users.noreply.github.com>
2024-05-30 09:31:56 +0200

    layout

--- app/compte/layout.tsx
+++ app/compte/layout.tsx
@@ -1,13 +1,16 @@
 export default function compteLayout({
-   children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block text-center justify-center">
-        {children}
-      </div>
-    </section>
-  );
-}
+
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    return (
+      <section className="">
+        <div className="">
+          {children}
+        </div>
+      </section>
+    );
+  }
+  
+
