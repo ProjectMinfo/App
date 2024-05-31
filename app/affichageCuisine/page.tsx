ee27cbe59da123dfd2ae79c6e3fc2aab5e35933c
Louis CHARPENTIER <louis.charpentier@student.junia.com>
2024-05-31 14:12:51 +0200

    affichageCuisineEnd

--- app/affichageCuisine/page.tsx
+++ app/affichageCuisine/page.tsx
@@ -1,13 +1,12 @@
 'use client';
 import React, { useState, useEffect } from "react";
-import { getCommande, getComptes, postCommande } from "@/config/api"; // Assurez-vous que le chemin est correct pour vos appels API
-
+import { getCommande, getComptes } from "@/config/api"; // Assurez-vous que le chemin est correct pour vos appels API
 import { NewCommandes, Comptes } from "@/types"; // Assurez-vous que le chemin est correct pour vos types
 
-type ColoredCommande = NewCommandes & { color: string };
+type ColoredCommande = NewCommandes & { color: string } & { nom: string };
 
 const CommandesCuisine = () => {
-  const [ColoredCommande, setCommandes] = useState<ColoredCommande[]>([]);
+  const [commandes, setCommandes] = useState<ColoredCommande[]>([]);
   const [clients, setClients] = useState<Comptes[]>([]);
   const [loading, setLoading] = useState(true);
   const [currentTime, setCurrentTime] = useState(new Date());
@@ -19,7 +18,7 @@ const CommandesCuisine = () => {
         const fetchCommandes = await getCommande();
         const comptes = await getComptes();
 
-        const resultCommandes : ColoredCommande[] = fetchCommandes
+        const resultCommandes: ColoredCommande[] = fetchCommandes
           .filter((commande: ColoredCommande) => commande.payee && commande.contenu)
           .map((commande: ColoredCommande) => ({
             ...commande,
@@ -30,13 +29,13 @@ const CommandesCuisine = () => {
         setCommandes(resultCommandes);
         setClients(comptes);
       } catch (error) {
-        console.error("Erreur lors de la récupération des ColoredCommande ou des comptes :", error);
+        console.error("Erreur lors de la récupération des commandes ou des comptes :", error);
       } finally {
         setLoading(false);
       }
     };
 
-    fetchCommandes();    
+    fetchCommandes();
 
     // Mettre à jour l'heure chaque seconde
     const interval = setInterval(() => {
@@ -52,44 +51,13 @@ const CommandesCuisine = () => {
     '#FF5733CC', '#33FF57CC', '#3357FFCC', '#FF33A1CC', '#FF8333CC',
     '#33FFF5CC', '#F5FF33CC', '#8D33FFCC', '#33FF8DCC', '#FF3385CC'
   ];
-  
+
   let colorIndex = 0;
-  
+
   const generateRandomColor = () => {
     const color = colors[colorIndex];
     colorIndex = (colorIndex + 1) % colors.length;
     return color + '80'; // Ajouter une transparence de 50%
-  };
-  
-
-  // Fonction pour valider un élément d'une commande
-  const handleValiderElement = async (id: number, type: 'chaud' | 'froid', produit: string) => {
-    try {
-      const updatedCommandes = ColoredCommande.map(commande => {
-        if (commande.id === id) {
-          const updatedContenu = commande.contenu.split('//').map(part => {
-            if (part.includes(produit)) {
-              return '';
-            }
-            return part;
-          }).join('//').replace(/\/\/+/g, '//').replace(/^\//, '').replace(/\/$/, '');
-
-          const isDistribuee = updatedContenu === '';
-
-          const newCommande : ColoredCommande= {...commande};
-          newCommande.contenu = updatedContenu;
-          newCommande.distribuee = isDistribuee;
-          postCommande(newCommande);
-
-          return { ...commande, contenu: updatedContenu, distribuee: isDistribuee };
-        }
-        return commande;
-      }).filter(commande => !commande.distribuee); // Filtrer les ColoredCommande distribuées
-
-      setCommandes(updatedCommandes);
-    } catch (error) {
-      console.error("Erreur lors de la validation de l'élément :", error);
-    }
   };
 
   const toggleFullscreen = () => {
@@ -128,19 +96,13 @@ const CommandesCuisine = () => {
         <div className="w-1/2 p-2 border-r border-gray-300 overflow-hidden">
           <h2 className="text-2xl text-blue-800 font-bold mb-4">Sandwichs et Hot-Dogs</h2>
           <div className="grid grid-cols-1 gap-2">
-            {ColoredCommande.filter(commande => commande.contenu.includes('Sandwich') || commande.contenu.includes('Hot-Dog')).slice(0, 10).map((commande, index) => {
+            {commandes.filter(commande => commande.contenu.includes('Sandwich') || commande.contenu.includes('Hot-Dog')).slice(0, 10).map((commande, index) => {
               const produitsFroids = commande.contenu.split('//').filter(part => part.includes('Sandwich') || part.includes('Hot-Dog'));
               return produitsFroids.map((produit, idx) => (
                 <div key={`${index}-${idx}`} className="p-2 rounded shadow-lg bg-white bg-opacity-80 text-black" style={{ backgroundColor: commande.color, height: '80px', width: '95%' }}>
-                  <h3 className="text-md font-semibold truncate">{commande.numCompte}</h3>
+                  <h3 className="text-md font-semibold truncate">{commande.nom}</h3>
                   <p className="text-sm mt-1 truncate">{produit}</p>
                   <p className="text-xs text-black mt-1 truncate">{commande.commentaire}</p>
-                  <button
-                    className="mt-2 px-2 py-1 bg-green-500 text-white text-xs rounded hover:bg-green-600"
-                    onClick={() => handleValiderElement(commande.id, 'froid', produit)}
-                  >
-                    Valider
-                  </button>
                 </div>
               ));
             })}
@@ -149,19 +111,13 @@ const CommandesCuisine = () => {
         <div className="w-1/2 p-2 overflow-hidden">
           <h2 className="text-2xl text-red-800 font-bold mb-4">Paninis et Croque-Monsieur</h2>
           <div className="grid grid-cols-1 gap-2">
-            {ColoredCommande.filter(commande => commande.contenu.includes('Panini') || commande.contenu.includes('Croque-Monsieur')).slice(0, 10).map((commande, index) => {
+            {commandes.filter(commande => commande.contenu.includes('Panini') || commande.contenu.includes('Croque-Monsieur')).slice(0, 10).map((commande, index) => {
               const produitsChauds = commande.contenu.split('//').filter(part => part.includes('Panini') || part.includes('Croque-Monsieur'));
               return produitsChauds.map((produit, idx) => (
                 <div key={`${index}-${idx}`} className="p-2 rounded shadow-lg bg-white bg-opacity-80 text-black" style={{ backgroundColor: commande.color, height: '80px', width: '95%' }}>
-                  <h3 className="text-md font-semibold truncate">{commande.numCompte}</h3>
+                  <h3 className="text-md font-semibold truncate">{commande.nom}</h3>
                   <p className="text-sm mt-1 truncate">{produit}</p>
                   <p className="text-xs text-black mt-1 truncate">{commande.commentaire}</p>
-                  <button
-                    className="mt-2 px-2 py-1 bg-green-500 text-white text-xs rounded hover:bg-green-600"
-                    onClick={() => handleValiderElement(commande.id, 'chaud', produit)}
-                  >
-                    Valider
-                  </button>
                 </div>
               ));
             })}
